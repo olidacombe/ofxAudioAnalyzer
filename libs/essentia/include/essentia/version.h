7557d58214da7b6266594d41aaba4b2749d99ed3
Oli Dacombe <olidacombe@gmail.com>
2016-12-23 13:55:39 +0000

    running without crash against essentia I built (just build the latest recommended tag.  So now I can write essentia algorithms tailored

--- libs/essentia/include/essentia/version.h
+++ libs/essentia/include/essentia/version.h
@@ -1,6 +1,6 @@
 
 #ifndef VERSION_H_
 #define VERSION_H_
-#define ESSENTIA_VERSION "2.1-dev"
-#define ESSENTIA_GIT_SHA "5bfe18e"
+#define ESSENTIA_VERSION "2.1-beta3"
+#define ESSENTIA_GIT_SHA "v2.1_beta3"
 #endif /* VERSION_H_ */
